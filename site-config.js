ff69186f3514e85a8786b7275a9d119af1dc189c
bogdanpotir <bogdan.potir@gmail.com>
2026-09-08 15:40:45 +0200

    Aggiorna cache dei puntini animati

--- site-config.js
+++ site-config.js
@@ -6,7 +6,7 @@ window.SM_CONFIG={imageBase:'smimages/',imageExtensions:['webp','png','jpg','jpe
   document.head.appendChild(link);
   var dots=document.createElement('link');
   dots.rel='stylesheet';
-  dots.href='dots.css?v=1';
+  dots.href='dots.css?v=2';
   document.head.appendChild(dots);
   var themeMeta=document.querySelector('meta[name="theme-color"]');
   if(themeMeta) themeMeta.setAttribute('content','#E85C8A');
