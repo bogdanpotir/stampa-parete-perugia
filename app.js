7d3cf515c0e4ba28c19abcb9c5dacc5e333b7cef
bogdanpotir <bogdan.potir@gmail.com>
2026-09-09 21:27:51 +0200

    Collega video 3 a YouTube Shorts

--- app.js
+++ app.js
@@ -1,5 +1,6 @@
 (()=>{
   const C=window.SM_CONFIG||{};
+  C.videos=Object.assign({},C.videos,{video3:'https://www.youtube.com/shorts/hOgHNrBdajQ'});
   const FORM_BOOKING_URL=C.formBookingUrl;
   const PRICE_PER_SQM=C.pricePerSqm||80;
   const MIN_BILLABLE_SQM=C.minimumBillableSqm||1;
